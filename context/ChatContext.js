e6b12abc5d9e8de5a2909992ed136d5da4a3c642
Rafael Pilumyan <Rafael.pilumyan@gmail.com>
2023-02-06 20:21:19 +0200

    Connect to stream chat SDK and create a user

--- context/ChatContext.js
+++ context/ChatContext.js
@@ -1,13 +1,53 @@
-import { createContext, useContext } from "react";
+import { useUserData } from "@nhost/react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
 
 export const ChatContext = createContext({});
 
 const ChatContectProvider = ({ children }) => {
-  const value = {username: "Veikia.lt"};
+  // component
+  const [chatClient, setChatClient] = useState();
+
+  const user = useUserData();
+
+  useEffect(() => {
+    const initChat = async () => {
+      if (!user) {
+        return;
+      }
+
+      const client = StreamChat.getInstance("hfuvrfq36x2z");
+
+      // get information about the authenticated user
+      // connect the user to stream chat
+      await client.connectUser(
+        {
+          id: user.id,
+          name: user.displayName,
+          image: user.avatarUrl,
+        },
+        client.devToken(user.id)
+      );
+
+      setChatClient(client);
+    };
+
+    initChat();
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (chatClient) {
+        chatClient.disconnectUser();
+      }
+    };
+  }, []);
+
+  const value = { username: "Veikia.lt" };
 
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
 };
 
-export const useChatContext = () => useContext(ChatContext)
+export const useChatContext = () => useContext(ChatContext);
 
 export default ChatContectProvider;
