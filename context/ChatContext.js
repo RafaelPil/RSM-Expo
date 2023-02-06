59f7e895a6f5ccec43f75ad2a9acceaf374620c0
Rafael Pilumyan <Rafael.pilumyan@gmail.com>
2023-02-06 21:15:12 +0200

    Chat UI

--- context/ChatContext.js
+++ context/ChatContext.js
@@ -1,12 +1,15 @@
 import { useUserData } from "@nhost/react";
 import { createContext, useContext, useEffect, useState } from "react";
-import { StreamChat } from "stream-chat";
+import { ActivityIndicator } from "react-native";
+import { StreamChat, Channel } from "stream-chat";
+import { OverlayProvider, Chat } from "stream-chat-expo";
 
 export const ChatContext = createContext({});
 
 const ChatContectProvider = ({ children }) => {
   // component
   const [chatClient, setChatClient] = useState();
+  const [currentChannel, setCurrentChannel] = useState();
   const [isConnected, setIsConnected] = useState(false);
 
   const user = useUserData();
@@ -35,6 +38,8 @@ const ChatContectProvider = ({ children }) => {
 
       const globalChannel = client.channel("livestream", "global", {
         name: "RSM",
+        image:
+          "https://img.freepik.com/premium-photo/futuristic-cyber-cat-cyberpunk-style-digital-art-style-illustration-painting_743201-3266.jpg",
       });
 
       await globalChannel.watch();
@@ -49,9 +54,18 @@ const ChatContectProvider = ({ children }) => {
     };
   }, [chatClient]);
 
-  const value = { username: "Veikia.lt" };
+  if (!chatClient) {
+    return <ActivityIndicator />;
+  }
 
-  return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
+  const value = { currentChannel, setCurrentChannel, chatClient };
+  return (
+    <OverlayProvider>
+      <Chat client={chatClient}>
+        <ChatContext.Provider value={value}>{children}</ChatContext.Provider>
+      </Chat>
+    </OverlayProvider>
+  );
 };
 
 export const useChatContext = () => useContext(ChatContext);
